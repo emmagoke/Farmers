29ed3f28a479d59aee149409b677ea1557711066
Emmanuel Olagoke <emma.goke05@gmail.com>
2024-07-13 02:11:05 +0100

    feature: import the module for custom user in django

--- users/models.py
+++ users/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+	AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
 
 # Create your models here.
