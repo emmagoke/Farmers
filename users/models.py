a65e51d53dd954f062bb82254ae25614258a57a3
Emmanuel Olagoke <emma.goke05@gmail.com>
2024-07-13 02:21:34 +0100

    feature: created the UserManager For the User Model

--- users/models.py
+++ users/models.py
@@ -15,6 +15,39 @@ class TimeStamp(models.Model):
         abstract = True
 
 
+class UserManager(BaseUserManager):
+    def create_user(self, first_name, last_name, email, password=None, **extra_fields):
+        if first_name is None:
+            raise TypeError("User must have first_name")
+        if last_name is None:
+            raise TypeError("User must have last_name")
+        if email is None:
+            raise TypeError("User must have email.")
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+        self, first_name, last_name, email, password=None, **extra_fields
+    ):
+        user = self.create_user(first_name, last_name, email, password, **extra_fields)
+
+        user.is_superuser = True
+        user.is_admin = True
+        user.is_active = True
+        user.is_verified = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
 class CustomUser(AbstractUser):
 	email = models.CharField(max_length=254)
 	phone_number = models.CharField(max_length=14, unique=True)
