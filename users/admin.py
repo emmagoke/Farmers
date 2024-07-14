8f94aa8f26a2598132c167b3d3555f6ca9c0f911
Emmanuel Olagoke <emma.goke05@gmail.com>
2024-07-14 17:49:35 +0100

    feature: added the necessary import for admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import CustomUser
