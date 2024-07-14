c48909486f3efdab5f86912721fcb190edf719ab
Emmanuel Olagoke <emma.goke05@gmail.com>
2024-07-14 18:42:11 +0100

    feature: added the add_fieldsets to create the user

--- users/admin.py
+++ users/admin.py
@@ -34,8 +34,31 @@ class CustomUserAdmin(UserAdmin):
 				)
 			})
 	)
-	# add_fieldsets = UserAdmin.add_fieldsets + ((None,
-	# 	{"fields": ("phone_number", "email", )}), )
+	# This is the field displayed when a new user is to be created
+	# on the django admin
+	add_fieldsets = (
+		(
+			None, 
+			{
+				"fields": (
+					"phone_number", "email",
+					"first_name", "last_name",
+					"password1", "password2",
+				)
+			}
+		),
+		(
+			"Permissions",
+			{
+				"fields": (
+					"is_admin",
+					"is_verified",
+					"is_active",
+					"is_staff",
+				)
+			}
+		),
+	)
 	ordering = ("email",)
 
 admin.site.register(CustomUser, CustomUserAdmin)
