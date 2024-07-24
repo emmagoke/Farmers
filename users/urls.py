9415e3eced8d1a8e3e81b9fd2f9720f9b110e33d
Emmanuel Olagoke <emma.goke05@gmail.com>
2024-07-24 18:58:41 +0100

    refactor: made changes to the url file

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from .viewsets import AuthenticationViewset
 
@@ -9,4 +13,6 @@ router.register(r"auth", AuthenticationViewset, basename="auth")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path('auth/login/', TokenObtainPairView.as_view(), name='login'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='refresh'),
 ]
