0df5be6d9b6f9f0b67cba4050971c8846c5fdf6d
Emmanuel Olagoke <emma.goke05@gmail.com>
2024-07-24 16:21:04 +0100

    refactor: removed the JWT from the users app

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 from .viewsets import AuthenticationViewset
 
