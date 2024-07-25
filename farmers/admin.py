d078f592b81880dff7e55669eaf8af9629701758
Emmanuel Olagoke <emma.goke05@gmail.com>
2024-07-25 02:23:12 +0100

    feature: changted the age field to birth_date

--- farmers/admin.py
+++ farmers/admin.py
@@ -4,6 +4,6 @@ from .models import Farmer
 # Register your models here.
 
 class FarmerAdmin(admin.ModelAdmin):
-	list_display = ['first_name', 'last_name', 'age']
+	list_display = ['first_name', 'last_name', 'birth_date']
 
 admin.site.register(Farmer, FarmerAdmin)
--- farmers/models.py
+++ farmers/models.py
@@ -7,7 +7,7 @@ class Farmer(models.Model):
 	first_name = models.CharField(max_length=50)
 	last_name = models.CharField(max_length=50)
 	phone_number = models.CharField(max_length=14, unique=True)
-	age = models.PositiveIntegerField()
+	birth_date = models.DateTimeField(null=True)
 	address = models.CharField(max_length=150)
 	crops = models.CharField(max_length=150)
 	season_best_for_crops = models.CharField(max_length=15)
