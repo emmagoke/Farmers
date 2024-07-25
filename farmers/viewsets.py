cb8ee4724cec28b452a80cfe8c99531ebfe632df
Emmanuel Olagoke <emma.goke05@gmail.com>
2024-07-25 15:26:24 +0100

    feature: created a farmer viewset

---
+++ farmers/viewsets.py
@@ -0,0 +1,25 @@
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+
+from core.response_manager import ResponseManager
+from .services import FarmerService
+
+
+class FarmersViewset(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+
+    @action(
+        detail=False,
+        methods=["post"],
+        url_path="create",
+    )
+    def create_farmers(self, request):
+        response = FarmerService.create_farmer_service(request)
+
+        return ResponseManager.handle_response(
+                errors=response.get("errors", None),
+                message=response.get("message", None),
+                status=response.get("status"),
+                data=response.get("data", None)
+            )
