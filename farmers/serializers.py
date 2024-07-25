874021d66dcb2e1c78aca936bd0a709632c9c483
Emmanuel Olagoke <emma.goke05@gmail.com>
2024-07-25 13:18:42 +0100

    feature: added the field for RegisterFarmerSerializer

--- farmers/serializers.py
+++ farmers/serializers.py
@@ -16,15 +16,14 @@ class FarmerSerializer(serializers.ModelSerializer):
 
 
 class RegisterFarmerSerializer(serializers.Serializer):
-
-	class Meta:
-		model = Farmer
-		fields = [
-			'id', 'first_name',
-			'last_name', 'phone_number',
-			'age', 'address',
-			'crops', 'season_best_for_crops', 'user_id'
-		]
+	first_name = serializers.CharField(max_length=50)
+	last_name = serializers.CharField(max_length=50)
+	phone_number = serializers.CharField(max_length=14)
+	birth_date = serializers.DateTimeField()
+	address = serializers.CharField(max_length=150)
+	crops = serializers.CharField(max_length=150)
+	season_best_for_crops = serializers.CharField(max_length=15)
+	# user_id = serializers.PrimaryKeyRelatedField(query=User.objects.all())
 
 	def create(self, validated_data):
 		farmer_list = Farmer(**validated_data)
