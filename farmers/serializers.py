cda28e016f38a9339f63824d68776061a152c1e1
Emmanuel Olagoke <emma.goke05@gmail.com>
2024-07-26 12:02:32 +0100

    feature: added the fields for the FarmerSerializer

--- farmers/serializers.py
+++ farmers/serializers.py
@@ -4,16 +4,17 @@ from rest_framework.validators import UniqueValidator
 from .models import Farmer
 from users.helpers import filter_phone_number
 
-class FarmerSerializer(serializers.ModelSerializer):
-	user_id = serializers.IntegerField(write_only=True)
-	class Meta:
-		model = Farmer
-		fields = [
-			'id', 'first_name',
-			'last_name', 'phone_number',
-			'age', 'address',
-			'crops', 'season_best_for_crops', 'user_id'
-		]
+
+class FarmerSerializer(serializers.Serializer):
+	id = serializers.IntegerField(read_only=True)
+	first_name = serializers.CharField(max_length=50)
+	last_name = serializers.CharField(max_length=50)
+	phone_number = serializers.CharField(max_length=14)
+	birth_date = serializers.DateTimeField()
+	address = serializers.CharField(max_length=150)
+	crops = serializers.CharField(max_length=150)
+	season_best_for_crops = serializers.CharField(max_length=15)
+	user_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
 
 
 class RegisterFarmerSerializer(serializers.Serializer):
