94e2594446eaef58f87b1bab8a8b00ae6c973a19
Emmanuel Olagoke <emma.goke05@gmail.com>
2024-07-26 13:01:55 +0100

    feature: added the logic to get farmers

--- farmers/services.py
+++ farmers/services.py
@@ -1,6 +1,8 @@
 from django.db import transaction
 from django.db.utils import IntegrityError
 from rest_framework import  status
+from datetime import date
+from django.db.models import Q
 
 from .serializers import RegisterFarmerSerializer
 
@@ -35,4 +37,38 @@ class FarmerService:
                 message=str(e),
                 status=status.HTTP_400_BAD_REQUEST
             )
+    
+    @classmethod
+    def get_farmer_service(cls, request):
+        user = request.user
+        crops = request.query_params.get('crops', None)
+        phone_number = request.query_params.get('phone_number',None)
+        min_age = request.query_params.get('min_age', None)
+        max_age = request.query_params.get('max_age')
+        farmers = Farmer.objects.filter(user=user)
+
+        filters = Q()
+
+        if crops:
+            filters &= Q(crops__icontains=crops) # And Condition or |= for OR
+        if phone_number:
+            filters &= Q(phone_number__icontains=phone_number)
+        if min_age:
+            today = date.today()
+            date_threshold = today.replace(year=today.year - int(min_age))
+            filters &= Q(birth_date__lte=date_threshold)
+        if max_age:
+            today = date.today()
+            date_threshold = today.replace(year=today.year - int(max_age))
+            filters &= Q(birth_date__gte=date_threshold)
         
+        print(filters)
+        farmers = farmers.filter(filters)
+
+        serializer = FarmerSerializer(farmers, many=True)
+
+        return dict(
+            queryset=farmers,
+            serializer_=FarmerSerializer,
+            status=status.HTTP_200_OK
+        )
