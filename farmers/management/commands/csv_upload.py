2d2faeee8a423962b8b8386ee8fdb82ff4d46ff6
Emmanuel Olagoke <emma.goke05@gmail.com>
2024-08-03 08:17:39 +0100

    refactor: refactor the code to upload to cloudinary

--- farmers/management/commands/csv_upload.py
+++ farmers/management/commands/csv_upload.py
@@ -5,46 +5,47 @@ return the link to the csv file on the console.
 """
 from django.core.management.base import BaseCommand, CommandError
 from farmers.models import Farmer
-import requests
 from datetime import datetime
-from os import getenv
+import os
+from cloudinary.uploader import upload
+from farmers.helper import calculate_age
 
 import csv
 
 class Command(BaseCommand):
 
-	def add_arguments(self, parser):
-		parser.add_argument('url', type="str",
-			help="This indicate the url you want to upload to")
+	# def add_arguments(self, parser):
+	# 	parser.add_argument('url', type=str,
+	# 		help="This indicate the url you want to upload to")
 
 
 	def handle(self, *args, **kwargs):
-		url = kwargs.get('url', None)
-		if url is None:
-			raise CommandError('You must supply the url fields')
+		# url = kwargs.get('url', None)
+		# if url is None:
+		# 	raise CommandError('You must supply the url fields')
 		farmers = Farmer.objects.all()
-		fields = [ field.name for field in Farmer._meta.get_fields()]
+		# fields = [ field.name for field in Farmer._meta.get_fields()]
 		file_name = "farmer_upload_{}_{}_{}.csv".format(
 			datetime.now().year, datetime.now().month, datetime.now().day)
 		with open(file_name, 'w') as file:
 			writer = csv.writer(file)
-			writer.writerow(fields[:-1])
+			writer.writerow(
+            	['ID', 'First Name', 'Last Name', 'Phone Number', 'Age', 'Address', 'Crops', 'Best Season']
+        	)
 
 			for farmer in farmers:
+				age = calculate_age(farmer.birth_date)
 				writer.writerow([farmer.id, farmer.first_name, farmer.last_name,
-					farmer.phone_number, farmer.age, farmer.address, farmer.crops,
+					farmer.phone_number, age, farmer.address, farmer.crops,
 					farmer.season_best_for_crops])
 		file_up = open(file_name, 'rb')
 
-		url = url
-		files = {"file": file_up}
-		username = getenv('USERNAME', '')
-		password = getenv('PASSWORD', '')
-		payload={username: username, password: password}
 		try:
-			response = requests.post(url, files=files, data=payload)
-			self.stdout(response.json()['url'])
+			resource_type = "raw" 
+			cloud_url = upload(file_up, folder="Farmers", resource_type=resource_type)
+			file_url = cloud_url["secure_url"]
+			self.stdout.write(file_url)
 		except Exception as error:
-			self.stdout.write(error)
+			self.stdout.write(str(error))
 		finally:
 			os.remove(file_name)
